d0bc05ec6ded00125b90b9ff6bc240a192f03ebb
vvakame <vvakame+dev@gmail.com>
2015-02-09 23:57:04 +0900

    feat(deps): add grunt-conventional-changelog

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,11 @@ module.exports = function (grunt) {
         // Unit tests.
         nodeunit: {
             tests: ['test/*_test.js']
+        },
+
+        changelog: {
+            options: {
+            }
         }
     });
 
@@ -55,6 +60,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-conventional-changelog');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
